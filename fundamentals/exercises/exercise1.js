9a8d0f0b3b8f9982477c38d8d98c2ca2ade3e8bc
wolfboy519 <brycedsheff@gmail.com>
2025-06-30 12:22:18 -0400

    Co-authored-by: Adam Sheff <adamISheff@users.noreply.github.com>

--- fundamentals/exercises/exercise1.js
+++ fundamentals/exercises/exercise1.js
@@ -1,11 +1,15 @@
-// 1. Create a variable called 'name' and assign it a string value
-
-// 2. Create a variable called 'age' and assign it a number value
-
-// 3. Create a variable called 'isStudent' and assign it a boolean value
-
+// // 1. Create a variable called 'name' and assign it a string value
+// let name = '10'
+// console.log(name + ' is a type of ' + typeof name)
+// // 2. Create a variable called 'age' and assign it a number value
+// let age = 44
+// console.log(age + ' is a ' + typeof age)
+// // 3. Create a variable called 'isStudent' and assign it a boolean value
+// let isStudent = true
+// console.log(isStudent + ' is a ' + typeof isStudent)
 // 4. Create a variable called 'hobbies' and assign it an array of strings
-
+const hobbies = ['hi',' greetings.',' bye',' see ya']
+console.log(hobbies)
 // 5. Create a variable called 'address' and assign it an object with the following properties: street, city, state
 
 // 6. Create a variable called 'colors' and assign it an array of strings
--- fundamentals/variables.js
+++ fundamentals/variables.js
@@ -1,25 +1,51 @@
-let number = 1;
+const number = 1;
 let string = "hello";
 let boolean = true;
 let nullValue = null;
 let undefinedValue = undefined;
 
+// number = 5;
+
 // console.log(number + ' is a type of ' + typeof number);
+// console.log('typeof this stentance is ' + typeof 'this sentence is');
+// console.log('');
+
+
+// number = 'ten';
+// console.log(number + ' is a type of ' + typeof number);
+// console.log('');
+
+
+// number = 10;
+// console.log(number + ' is a type of ' + typeof number);
+// console.log('');
+
+// number = '10';
+// console.log(number + ' is a type of ' + typeof number);
+// console.log('');
+
 // console.log(string + ' is a type of ' + typeof string);
 // console.log(boolean + ' is a type of ' + typeof boolean);
 // console.log(nullValue + ' is a type of ' + typeof nullValue);
 // console.log(undefinedValue + ' is a type of ' + typeof undefinedValue);
 
-const array = [1,2,3,4,5];
+const array = [1,2,3];
+// const anything = [1, true, 'word', number];
 
-console.log(array + ' is a type of ' + typeof array);
+// console.log(array + ' is a type of ' + typeof array);
 
-console.log(array);
-console.log(array[0]);
+// console.log(array.length);
+// console.log(mixedArray + ' is a type of ' + typeof mixedArray);
 
-array.forEach(element => {
-    console.log(element);
-});
+// console.log(array);
+// console.log(array[0]);
+// console.log(anything[2]);
+// console.log(array[1]);
+// console.log(array[3]);
+
+// array.forEach((bryce) => {
+//     console.log(bryce);
+// });
 
 
 const object = {
@@ -35,7 +61,7 @@ const object = {
     }
 };
 
-console.log(object + ' is a type of ' + typeof object);
+// console.log(object + ' is a type of ' + typeof object);
 
-console.log(object.name + ' lives in ' + object.address.city + ' and is ' + object.age + ' years old');
-console.log(object.city + ' is a type of ' + typeof object.city);
+// console.log(object.name + ' lives in ' + object.address.city + ' and is ' + object.age + ' years old');
+// console.log(object.city + ' is a type of ' + typeof object.city);
